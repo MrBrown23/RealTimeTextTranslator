801b4bba75562ef1342797ab7cb2ca4b90f8ec75
MrBrown23 <MRRbrown@protonmail.com>
2024-01-28 16:37:02 +0000

    Last Modifications

--- app/src/main/java/com/example/realtimetexttranslator/MainActivity.java
+++ app/src/main/java/com/example/realtimetexttranslator/MainActivity.java
@@ -1,21 +1,253 @@
 package com.example.realtimetexttranslator;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.Manifest.permission.CAMERA;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private String textToTranslate ="";
+    private String toTranslate = "translate";
+    private ImageView captureIV ;
+    private TextView resultIV;
+    private Button snapBtn, detectBtn;
+    private Bitmap imageBitmap;
+    private final int REQUEST_IMAGE_CAPTURE = 1;
+    private ProgressDialog progressDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        captureIV = findViewById(R.id.captureIV);
+        resultIV = findViewById(R.id.waiting_text);
+        snapBtn = findViewById(R.id.SnapBtn);
+        detectBtn = findViewById(R.id.detectBtn);
+//        textToTranslate = "Your text here";
+        snapBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(checkPermission()){
+                    captureImage();
+                }
+                else {
+                    requestPermission();
+                }
+            }
+        });
+
+        detectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                textToTranslate = detectText();
+                detectText();
+                if(!textToTranslate.equals("")){
+                    System.out.println("Sending===================================================================================================Sending");
+                    System.out.println("That is the text that we are sending :"+textToTranslate);
+                    Intent newActivity = new Intent(MainActivity.this,TranslateActivity.class);
+                    newActivity.putExtra("textToTranslate", textToTranslate);
+                    startActivity(newActivity);
+                }
+
+            }
+        });
     }
 
-    public void goScanner(View view){
-        Intent newActivity = new Intent(MainActivity.this,ScannerActivity.class);
-        startActivity(newActivity);
+    private boolean checkPermission(){
+        int cameraPermission = ContextCompat.checkSelfPermission(getApplicationContext(),CAMERA);
+        return cameraPermission == PackageManager.PERMISSION_GRANTED;
     }
+    private void captureImage(){
+        Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(takePicture.resolveActivity(getPackageManager())!=null){
+            startActivityForResult(takePicture, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+
+    private void requestPermission(){
+        int PERMISSION_CODE = 200;
+        ActivityCompat.requestPermissions(this, new String[]{CAMERA},PERMISSION_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(grantResults.length>0){
+            boolean cameraPermission = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+            if(cameraPermission){
+                Toast.makeText(this, "Permission Granted..", Toast.LENGTH_SHORT).show();
+                captureImage();
+            }
+            else {
+                Toast.makeText(this, "Permission Denied..", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            if (extras != null) {
+                imageBitmap = (Bitmap) extras.get("data");
+                captureIV.setImageBitmap(imageBitmap);
+            }
+        }
+    }
+
+
+    private void runTextRecognition(Bitmap bitmap) {
+        InputImage image = InputImage.fromBitmap(bitmap, 0);
+
+        TextRecognizerOptions options = TextRecognizerOptions.DEFAULT_OPTIONS;
+        TextRecognizer recognizer = TextRecognition.getClient(options);
+
+        ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+        progressDialog.setMessage("Detecting text...");
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+
+        Task<Text> result = recognizer.process(image)
+                .addOnSuccessListener(new OnSuccessListener<Text>() {
+                    @Override
+                    public void onSuccess(Text visionText) {
+                        progressDialog.dismiss();
+                        processTextRecognitionResult(visionText);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        progressDialog.dismiss();
+                        // Task failed with an exception
+                        toTranslate = "No Text";
+                        Toast.makeText(MainActivity.this, "Failed to detect text: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                        showManualTextEntryDialog("Text");
+                    }
+                });
+    }
+
+    private void detectText() {
+        if (imageBitmap != null) {
+            runTextRecognition(imageBitmap);
+            float scaleFactor = 1.5f;
+            scaleAndRunTextRecognition(imageBitmap, scaleFactor);
+
+        } else {
+            showManualTextEntryDialog("Image");
+        }
+    }
+
+    private void showManualTextEntryDialog(String s) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("No "+s+" Detected");
+        builder.setMessage("Do you want to enter text manually?");
+
+        // Add buttons
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked Yes, handle accordingly
+                Intent newActivity = new Intent(MainActivity.this, TranslateActivity.class);
+                newActivity.putExtra("textToTranslate", "");
+                startActivity(newActivity);
+                dialog.dismiss();
+//                showManualTextEntryEditTextDialog();
+            }
+        });
+
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.dismiss();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+
+    private void scaleAndRunTextRecognition(Bitmap originalBitmap, float scaleFactor) {
+        new AsyncTask<Bitmap, Void, Bitmap>() {
+            @Override
+            protected void onPreExecute() {
+                progressDialog = new ProgressDialog(MainActivity.this);
+                progressDialog.setMessage("Scaling image...");
+                progressDialog.setCancelable(false);
+                progressDialog.show();
+            }
+
+            @Override
+            protected Bitmap doInBackground(Bitmap... bitmaps) {
+                return scaleBitmap(bitmaps[0], scaleFactor);
+            }
+
+            @Override
+            protected void onPostExecute(Bitmap scaledBitmap) {
+                progressDialog.dismiss();
+                runTextRecognition(scaledBitmap);
+            }
+        }.execute(originalBitmap);
+    }
+
+    private Bitmap scaleBitmap(Bitmap originalBitmap, float scaleFactor) {
+        int newWidth = (int) (originalBitmap.getWidth() * scaleFactor);
+        int newHeight = (int) (originalBitmap.getHeight() * scaleFactor);
+        return Bitmap.createScaledBitmap(originalBitmap, newWidth, newHeight, true);
+    }
+
+
+    private void processTextRecognitionResult(Text visionText) {
+
+        ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+        progressDialog.setMessage("Detecting text...");
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+        StringBuilder resultText = new StringBuilder();
+        for (Text.TextBlock block : visionText.getTextBlocks()) {
+            for (Text.Line line : block.getLines()) {
+                resultText.append(line.getText()).append("\n");
+            }
+            resultText.append("\n");
+        }
+        this.textToTranslate = resultText.toString();
+        System.out.println("Result===================================================================================================Result");
+        System.out.println(textToTranslate);
+        progressDialog.dismiss();
+    }
+
 }
--- app/src/main/java/com/example/realtimetexttranslator/TranslateActivity.java
+++ app/src/main/java/com/example/realtimetexttranslator/TranslateActivity.java
@@ -1,28 +1,268 @@
 package com.example.realtimetexttranslator;
 
+
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+
+
+
+import com.google.android.gms.tasks.Task;
+import  com.google.mlkit.nl.translate.Translation;
+import com.google.mlkit.nl.translate.Translator;
+import  com.google.mlkit.nl.translate.TranslatorOptions;
+
+
+import java.util.ArrayList;
+import java.util.Locale;
+
 
 public class TranslateActivity extends AppCompatActivity {
-    String[] fromLanguages = {"From","English", "Arabic", "French", "German"};
-    String[] toLanguages = {"TO","English", "Arabic", "French", "German"};
 
-//    private String inputText;
-//
-//    // Constructor that accepts a string
-//    public TranslateActivity(String inputText) {
-//        this.inputText = inputText;
-//    }
+    private Spinner fromSpinner, toSpinner;
+    private TextInputEditText sourceText;
+    private ImageView mieIV;
+    private MaterialButton translateBtn;
+    private ImageView appLogo;
+    private TextView translateIV;
+
+    String[] fromLanguage = {"To", "English", "Afrikaans", "Arabic", "Belarusian", "Bulgarian", "Bengali", "Catalan", "Czech", "French", "German",  "Hindi", "Spanish", "Urdu", "Welch"};
+    String[] toLanguage = {"from", "English", "Afrikaans", "Arabic", "Belarusian", "Bulgarian", "Bengali", "Catalan", "Czech",  "French", "German", "Hindi","Spanish", "Urdu", "Welch"};
+
+    private static final int REQUEST_PERMISSION_CODE = 1;
+    String languageCode, fromLanguageCode, toLanguageCode;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transnlate);
-         String inputText = getIntent().getStringExtra("textToTranslate");
-        if(inputText!=null){
+        fromSpinner = findViewById(R.id.idFromSpinner);
+        toSpinner = findViewById(R.id.idToSpinner);
+        sourceText = findViewById(R.id.sourceEditText);
+        translateBtn = findViewById(R.id.translationBtn);
+        translateIV = findViewById(R.id.TranslatedTextIV);
+        mieIV = findViewById(R.id.miceIV);
+        appLogo = findViewById(R.id.appLogo);
+
+        String inputText = getIntent().getStringExtra("textToTranslate");
+        System.out.println("Here is the inputshit"+inputText);
+        System.out.println("Test===================================================================================================Test");
+        if(!inputText.equals("")){
+            System.out.println("===================================================================================================");
+            sourceText.setText(inputText);
             System.out.println(inputText);
         }
+
+        fromSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                fromLanguageCode = getLanguageCode(fromLanguage[i]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        ArrayAdapter fromAdapter = new ArrayAdapter(this, R.layout.spinner_item, fromLanguage);
+        fromAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        fromSpinner.setAdapter(fromAdapter);
+
+
+
+        toSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                toLanguageCode = getLanguageCode(toLanguage[i]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        ArrayAdapter toAdapter = new ArrayAdapter(this, R.layout.spinner_item, toLanguage);
+        toAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        toSpinner.setAdapter(toAdapter);
+
+
+        mieIV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+                intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Say something to translate");
+                try {
+                    startActivityForResult(intent, REQUEST_PERMISSION_CODE);
+                }catch (Exception e){
+                    e.printStackTrace();
+                    Toast.makeText(TranslateActivity.this, ""+e.getMessage(),Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        appLogo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent newActivity = new Intent(TranslateActivity.this,MainActivity.class);
+                startActivity(newActivity);
+            }
+        });
+
+        translateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                translateIV.setVisibility(View.VISIBLE);
+                translateIV.setText("");
+                if(sourceText.getText().toString().isEmpty()){
+                    Toast.makeText(TranslateActivity.this, "Please enter text to translate", Toast.LENGTH_SHORT).show();
+                }
+                else if(fromLanguageCode == "none"){
+                    Toast.makeText(TranslateActivity.this, "Please select Source Language", Toast.LENGTH_SHORT).show();
+
+                }
+                else if(toLanguageCode == "none"){
+                    Toast.makeText(TranslateActivity.this, "Please select Target Language", Toast.LENGTH_SHORT).show();
+
+                }
+                else {
+                    translateText(fromLanguageCode, toLanguageCode, sourceText.getText().toString());
+                }
+            }
+        });
+
+
+
+    }
+
+    private void translateText(String fromLanguageCode, String toLanguageCode, String source) {
+        translateIV.setText("Downloading model please wait!");
+        System.out.println(source);
+        System.out.println(fromLanguageCode+toLanguageCode);
+        try{
+            TranslatorOptions options = new TranslatorOptions.Builder()
+                    .setTargetLanguage(toLanguageCode)
+                    .setSourceLanguage(fromLanguageCode)
+                    .build();
+            Translator translator = Translation.getClient(options);
+
+            String sourceString = source;
+            ProgressDialog progressDialog = new ProgressDialog(TranslateActivity.this);
+            progressDialog.setMessage("Downloading the translation model...");
+            progressDialog.setCancelable(false);
+            progressDialog.show();
+
+            translator.downloadModelIfNeeded().addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void unused) {
+                    progressDialog.dismiss();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    progressDialog.dismiss();
+                }
+            });
+
+
+            Task<String> result = translator.translate(sourceString).addOnSuccessListener(new OnSuccessListener<String>() {
+                @Override
+                public void onSuccess(String s) {
+                    translateIV.setText(s);
+//                            Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(TranslateActivity.this, e.getMessage().toString(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }catch (Exception exception){
+            Toast.makeText(TranslateActivity.this, ""+exception.getMessage(),Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_PERMISSION_CODE){
+            ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            sourceText.setText(result.get(0));
+        }
+    }
+    //    String[] toLanguage = {"from", "English", "Afrikaans", "Arabic", "Belarusian", "Bulgarian", "Bengali", "Catalan", "Czech",  "French", "German", "Hindi","Spanish", "Urdu", "Welch"};
+
+    private String getLanguageCode(String language) {
+        String languageCode = "none";
+        switch (language){
+            case "English":
+                languageCode = "en";
+                break;
+            case "Afrikaans":
+                languageCode = "af";
+                break;
+            case "Arabic":
+                languageCode = "ar";
+                break;
+            case "Belarusian":
+                languageCode = "be";
+                break;
+            case "Bulgarian":
+                languageCode = "bg";
+                break;
+            case "Bengali":
+                languageCode = "bn";
+                break;
+            case "Catalan":
+                languageCode = "ca";
+                break;
+            case "Czech":
+                languageCode = "cs";
+                break;
+            case "French":
+                languageCode = "fr";
+                break;
+            case "German":
+                languageCode = "de";
+                break;
+            case "Hindi":
+                languageCode = "hi";
+                break;
+            case "Spanish":
+                languageCode = "es";
+                break;
+            case "Urdu":
+                languageCode = "ur";
+                break;
+            case "Welch":
+                languageCode = "cy";
+                break;
+            default:
+                languageCode="none";
+        }
+        return languageCode;
     }
 }
